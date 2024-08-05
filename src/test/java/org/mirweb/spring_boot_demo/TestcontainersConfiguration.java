1f46ec1e1beefcc453695878163cb9fe370a5e63
Mirko Weber <mirko@wils.tk>
2024-08-05 21:36:33 +0200

    feat: inital commit
    project created out of [Spring Starter](https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.3.2&packaging=jar&jvmVersion=21&groupId=org.mirweb&artifactId=spring-boot-demo&name=spring-boot-demo&description=Demo%20project%20for%20Spring%20Boot&packageName=org.mirweb.spring-boot-demo&dependencies=devtools,docker-compose,testcontainers,web)

---
+++ src/test/java/org/mirweb/spring_boot_demo/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package org.mirweb.spring_boot_demo;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
