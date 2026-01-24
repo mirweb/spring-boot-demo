54285206185abbf900e65fbf95f2bd966a0fb557
Mirko Weber <mirko@wils.tk>
2026-01-24 22:45:42 +0100

    fix: use spring-boot-starter-test for webmvc tests

--- src/test/java/org/mirweb/spring_boot_demo/SpringBootDemoApplicationTests.java
+++ src/test/java/org/mirweb/spring_boot_demo/SpringBootDemoApplicationTests.java
@@ -1,15 +1,38 @@
 package org.mirweb.spring_boot_demo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@WebMvcTest(SpringBootDemoApplication.class)
 class SpringBootDemoApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void helloDefaultsToWorld() throws Exception {
+		mockMvc.perform(get("/api/hello"))
+			.andExpect(status().isOk())
+			.andExpect(content().string("Hello World!"));
+	}
+
+	@Test
+	void helloUsesNameQueryParam() throws Exception {
+		mockMvc.perform(get("/api/hello").param("name", "Alice"))
+			.andExpect(status().isOk())
+			.andExpect(content().string("Hello Alice!"));
 	}
 
 }
